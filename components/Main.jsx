c465a65acf4fd0bdc3531b66311acc9df3216f10
Bharat Kumar <kumarbharat661@gmail.com>
2022-03-22 20:36:59 +0530

    changed hieght of main section to auto

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ import Work from "./Work";
 
 const Main = () => {
   return (
-    <div className="h-[1200vh] bg-yellow-700 grow">
+    <div className="bg-yellow-700 grow">
       <Home />
       <Skills />
       <Work />
